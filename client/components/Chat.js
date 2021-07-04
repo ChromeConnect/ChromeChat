2ded056965126cf5e970246b2e8345273e366e5a
Hernan Berisso <hberisso1@gmail.com>
2021-07-04 00:13:22 -0400

    fix: modified styling for chat room

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,47 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 /**
  * COMPONENT
  */
 const Chat = () => {
-  var socket = io();
+	var socket = io()
 
-  const [chatMessages, setMessages] = useState([]);
+	const [chatMessages, setMessages] = useState([])
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    let msg = e.target.input.value;
-    if (msg) {
-      socket.emit("chat message", msg);
-      e.target.input.value = "";
-      //send msg to firebase chat topic
-    }
-  }
+	function handleSubmit(e) {
+		e.preventDefault()
+		let msg = e.target.input.value
+		if (msg) {
+			socket.emit("chat message", msg)
+			e.target.input.value = ""
+			//send msg to firebase chat topic
+		}
+	}
 
-  //on first load, or refresh
-  //loop through messages state and display all messages
-  useEffect(() => {
-    console.log("fetch all messages with firebase and display");
-  }, []);
+	//on first load, or refresh
+	//loop through messages state and display all messages
+	useEffect(() => {
+		console.log("fetch all messages with firebase and display")
+	}, [])
 
-  //get url topic
-  //url topic will be used inside .on('url topic')
-  socket.on("chat message", function (msg) {
-    var messages = document.getElementById("messages");
-    var item = document.createElement("li");
-    item.textContent = msg;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
-  });
+	//get url topic
+	//url topic will be used inside .on('url topic')
+	socket.on("chat message", function (msg) {
+		var messages = document.getElementById("messages")
+		var item = document.createElement("p")
+		item.textContent = msg
+		messages.appendChild(item)
+		window.scrollTo(0, document.body.scrollHeight)
+	})
 
-  return (
-    <div>
-      <ul id="messages"></ul>
-      <form id="form" onSubmit={handleSubmit}>
-        <input id="input" autoComplete="off" />
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  );
-};
+	return (
+		<div>
+			<div class="header" id="myHeader">
+				<h2>Topic</h2>
+			</div>
+			<div id="messages"></div>
+			<form id="form" onSubmit={handleSubmit}>
+				<input id="input" autoComplete="off" />
+				<button type="submit">Send</button>
+			</form>
+		</div>
+	)
+}
 
-export default Chat;
+export default Chat
