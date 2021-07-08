fda90b21654a7827909f5360be77642228f8fb5c
Pablo Lopez <devpablolopez@gmail.com>
2021-07-08 08:58:07 -0700

    fixed merge conflicts

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -6,8 +6,8 @@ import {
   RichUtils,
   convertToRaw,
   convertFromRaw,
-	ContentState,
-	Modifier
+  ContentState,
+  Modifier,
 } from "draft-js";
 import { convertToHTML } from "draft-convert";
 import "draft-js/dist/Draft.css";
@@ -20,37 +20,38 @@ let userName = null;
 const filter = new Filter();
 filter.addWords("Flatiron", "General", "Assembly");
 
-const removeSelectedBlocksStyle = (editorState)  => {
-	const newContentState = RichUtils.tryToRemoveBlockStyle(editorState);
-	if (newContentState) {
-			return EditorState.push(editorState, newContentState, 'change-block-type');
-	}
-	return editorState;
-}
+const removeSelectedBlocksStyle = (editorState) => {
+  const newContentState = RichUtils.tryToRemoveBlockStyle(editorState);
+  if (newContentState) {
+    return EditorState.push(editorState, newContentState, "change-block-type");
+  }
+  return editorState;
+};
 
 const getResetEditorState = (editorState) => {
-	const blocks = editorState
-			.getCurrentContent()
-			.getBlockMap()
-			.toList();
-	const updatedSelection = editorState.getSelection().merge({
-			anchorKey: blocks.first().get('key'),
-			anchorOffset: 0,
-			focusKey: blocks.last().get('key'),
-			focusOffset: blocks.last().getLength(),
-	});
-	const newContentState = Modifier.removeRange(
-			editorState.getCurrentContent(),
-			updatedSelection,
-			'forward'
-	);
+  const blocks = editorState.getCurrentContent().getBlockMap().toList();
+  const updatedSelection = editorState.getSelection().merge({
+    anchorKey: blocks.first().get("key"),
+    anchorOffset: 0,
+    focusKey: blocks.last().get("key"),
+    focusOffset: blocks.last().getLength(),
+  });
+  const newContentState = Modifier.removeRange(
+    editorState.getCurrentContent(),
+    updatedSelection,
+    "forward"
+  );
 
-	const newState = EditorState.push(editorState, newContentState, 'remove-range');
-	return removeSelectedBlocksStyle(newState)
-}
+  const newState = EditorState.push(
+    editorState,
+    newContentState,
+    "remove-range"
+  );
+  return removeSelectedBlocksStyle(newState);
+};
 
 const Chat = () => {
-	var socket = io()
+  var socket = io();
 
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
@@ -66,46 +67,46 @@ const Chat = () => {
     return "not handled";
   };
 
-	function handleSubmit(e) {
-		e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
     const content = editorState.getCurrentContent();
-		let payload = {
-			msg: convertToRaw(content),
-			userName,
-			timestamp: new Date().toLocaleTimeString(),
-		}
-		if (content.hasText()) {
-			socket.emit("chat message", { payload, room })
-			setEditorState(getResetEditorState(editorState))
-		}
-	}
+    let payload = {
+      msg: convertToRaw(content),
+      userName,
+      timestamp: new Date().toLocaleTimeString(),
+    };
+    if (content.hasText()) {
+      socket.emit("chat message", { payload, room });
+      setEditorState(getResetEditorState(editorState));
+    }
+  }
 
-	useEffect(() => {
-		const { pathname } = history.location
-		let splitPathName = pathname.split("+")
-		userName = splitPathName[0].substring(1)
-		room = splitPathName[1].split("-").join(" ")
-		const topic = document.getElementById("topic")
-		topic.innerText = `${room[0].toUpperCase() + room.substring(1)}`
-		socket.emit("join", room)
-		console.log(room)
-	}, [])
+  useEffect(() => {
+    const { pathname } = history.location;
+    let splitPathName = pathname.split("+");
+    userName = splitPathName[0].substring(1);
+    room = splitPathName[1].split("-").join(" ");
+    const topic = document.getElementById("topic");
+    topic.innerText = `${room[0].toUpperCase() + room.substring(1)}`;
+    socket.emit("join", room);
+    console.log(room);
+  }, []);
 
-	socket.on("chat message", function (payload) {
-		var messages = document.getElementById("messages")
-		var item = document.createElement("div")
-		var sender = document.createElement("h5")
+  socket.on("chat message", function (payload) {
+    var messages = document.getElementById("messages");
+    var item = document.createElement("div");
+    var sender = document.createElement("h5");
     const msgFromRaw = convertFromRaw(payload.msg);
     let html = convertToHTML(msgFromRaw);
     html = filter.clean(html);
-		item.innerHTML = html
-		sender.textContent = `-${
-			payload.userName[0].toUpperCase() + payload.userName.substring(1)
-		}(sent at ${payload.timestamp})`
-		messages.appendChild(item)
-		messages.appendChild(sender)
-		window.scrollTo(0, document.body.scrollHeight)
-	})
+    item.innerHTML = html;
+    sender.textContent = `-${
+      payload.userName[0].toUpperCase() + payload.userName.substring(1)
+    }(sent at ${payload.timestamp})`;
+    messages.appendChild(item);
+    messages.appendChild(sender);
+    window.scrollTo(0, document.body.scrollHeight);
+  });
 
   function handleReturn(event) {
     if (event.shiftKey) {
@@ -118,7 +119,7 @@ const Chat = () => {
   return (
     <div>
       <div className="header" id="myHeader">
-        <h2>Lobby</h2>
+        <h2 id="topic"></h2>
       </div>
       <div id="messages"></div>
       <form id="form" onSubmit={handleSubmit}>
