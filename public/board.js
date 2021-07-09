5933a1e23ece64f4628eadcccc03ef403b2ea9ad
Hernan Berisso <hberisso1@gmail.com>
2021-07-09 18:08:33 -0400

    added clear button to whiteboard

--- public/board.js
+++ public/board.js
@@ -1,54 +1,69 @@
-let room = "lobby";
-let canvas = null;
-let ctx = null;
-let mouseDown = null;
-let x = null;
-let y = null;
-let colorOption = null;
-var socket = io();
-console.log(socket);
+let room = "lobby"
+let canvas = null
+let ctx = null
+let mouseDown = null
+let x = null
+let y = null
+let colorOption = null
+let width = 290
+let height = 140
+var socket = io()
 
-canvas = document.getElementById("canvas");
-const colorsPalette = document.getElementById("colorsPalette");
+canvas = document.getElementById("canvas")
+const colorsPalette = document.getElementById("colorsPalette")
 colorsPalette.addEventListener("click", function (e) {
-  colorOption = e.target.id;
-});
+	colorOption = e.target.id
+})
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx = canvas.getContext("2d");
-mouseDown = false;
-socket.emit("join", room);
+// const rectangle = document.getElementById("rectangleButton")
+// rectangle.addEventListener("click", function (e) {
+// 	console.log("reached rectangle")
+// 	ctx.beginPath()
+// 	ctx.rect(200, 200, 150, 100)
+// 	ctx.stroke()
+// })
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+ctx = canvas.getContext("2d")
+mouseDown = false
+socket.emit("join", room)
+
+const clear = document.getElementById("clearButton")
+clear.addEventListener("click", function (e) {
+	console.log("reached clear")
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
 
 socket.on("draw", function ({ x, y, colorOption }) {
-  ctx.lineTo(x, y);
-  ctx.stroke();
-  ctx.strokeStyle = colorOption;
-});
+	ctx.lineTo(x, y)
+	ctx.stroke()
+	ctx.strokeStyle = colorOption
+})
 socket.on("down", function ({ x, y }) {
-  ctx.moveTo(x, y);
-});
+	ctx.moveTo(x, y)
+})
 
 window.onmousedown = (e) => {
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  socket.emit("down", { payload: { x, y, colorOption }, room });
-  mouseDown = true;
-  ctx.closePath();
-};
+	ctx.beginPath()
+	ctx.moveTo(x, y)
+	socket.emit("down", { payload: { x, y, colorOption }, room })
+	mouseDown = true
+	ctx.closePath()
+}
 
 window.onmouseup = (e) => {
-  mouseDown = false;
-};
+	mouseDown = false
+}
 
 window.onmousemove = (e) => {
-  x = e.clientX;
-  y = e.clientY;
+	x = e.clientX
+	y = e.clientY
 
-  if (mouseDown) {
-    socket.emit("draw", { payload: { x, y, colorOption }, room });
-    ctx.lineTo(x, y);
-    ctx.stroke();
-    ctx.strokeStyle = colorOption;
-  }
-};
+	if (mouseDown) {
+		socket.emit("draw", { payload: { x, y, colorOption }, room })
+		ctx.lineTo(x, y)
+		ctx.stroke()
+		ctx.strokeStyle = colorOption
+	}
+}
