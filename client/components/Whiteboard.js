d5cec6b3d3308fa6110170284644cf11454c6b89
Hernan Berisso <hberisso1@gmail.com>
2021-07-09 12:35:05 -0400

    testing sizes

--- client/components/Whiteboard.js
+++ client/components/Whiteboard.js
@@ -1,56 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from "react"
+import history from "../history"
 
 const Whiteboard = () => {
-  var socket = io();
-  let room = null;
-  let canvas = null;
-  let ctx = null;
-  let mouseDown = null;
-  useEffect(() => {
-    canvas = document.getElementById("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx = canvas.getContext("2d");
-    mouseDown = false;
-    room = "lobby";
-    socket.emit("join", room);
-  }, []);
+	var socket = io()
+	let room = null
+	let canvas = null
+	let ctx = null
+	let mouseDown = null
+	useEffect(() => {
+		const { pathname } = history.location
+		room = pathname.split("-").join(" ")
+		canvas = document.getElementById("canvas")
+		canvas.width = window.innerWidth
+		canvas.height = window.innerHeight
+		ctx = canvas.getContext("2d")
+		mouseDown = false
+		socket.emit("join", room)
+	}, [])
 
-  let x;
-  let y;
+	let x
+	let y
 
-  socket.on("draw", function ({ x, y }) {
-    ctx.lineTo(x, y);
-    ctx.stroke();
-  });
-  socket.on("down", function ({ x, y }) {
-    ctx.moveTo(x, y);
-  });
+	socket.on("draw", function ({ x, y }) {
+		ctx.lineTo(x, y)
+		ctx.stroke()
+	})
+	socket.on("down", function ({ x, y }) {
+		ctx.moveTo(x, y)
+	})
 
-  window.onmousedown = (e) => {
-    ctx.moveTo(x, y);
-    socket.emit("down", { payload: { x, y }, room });
-    mouseDown = true;
-  };
+	window.onmousedown = (e) => {
+		ctx.moveTo(x, y)
+		socket.emit("down", { payload: { x, y }, room })
+		mouseDown = true
+	}
 
-  window.onmouseup = (e) => {
-    mouseDown = false;
-  };
+	window.onmouseup = (e) => {
+		mouseDown = false
+	}
 
-  window.onmousemove = (e) => {
-    x = e.clientX;
-    y = e.clientY;
+	window.onmousemove = (e) => {
+		x = e.clientX
+		y = e.clientY
 
-    if (mouseDown) {
-      socket.emit("draw", { payload: { x, y }, room });
-      ctx.lineTo(x, y);
-      ctx.stroke();
-    }
-  };
+		if (mouseDown) {
+			socket.emit("draw", { payload: { x, y }, room })
+			ctx.lineTo(x, y)
+			ctx.stroke()
+		}
+	}
 
-  return (
-    <canvas style={{ width: "100%", height: "100%" }} id="canvas"></canvas>
-  );
-};
+	return <canvas id="canvas"></canvas>
+}
 
-export default Whiteboard;
+export default Whiteboard
