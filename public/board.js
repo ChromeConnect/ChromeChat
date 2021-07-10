a9f5376ca37dd55b9804fdc3e3c1fc284dba0c85
Pablo Lopez <devpablolopez@gmail.com>
2021-07-10 12:20:40 -0700

    erase on board

--- public/board.js
+++ public/board.js
@@ -7,23 +7,43 @@ let x = null;
 let y = null;
 let colorOption = null;
 var socket = io();
-
+let circularCursorElement = document.getElementById("circularcursor");
+var inMemCanvas = document.createElement("canvas");
+var inMemCtx = inMemCanvas.getContext("2d");
 canvas = document.getElementById("canvas");
 const colorsPalette = document.getElementById("colorsPalette");
-
-colorsPalette.addEventListener("click", function (e) {
-  colorOption = e.target.id;
-});
-
 //options for rectangle, draw, text, etc.
 let options = null;
+
+//Color Selection
+colorsPalette.addEventListener("click", function (e) {
+  colorOption = e.target.id;
+  circularCursorElement.style.visibility = "hidden";
+});
+
+//Erase with mouse
+const eraserButton = document.getElementById("eraserButton");
+eraserButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  //change cursor and color and options
+  colorOption = "white";
+  options = "draw";
+  circularCursorElement.style.visibility = "visible";
+});
+
+//Erase entire white board
+const clear = document.getElementById("clearButton");
+clear.addEventListener("click", function (e) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 
 //Change option to text
 let textButton = document.getElementById("textButton");
 textButton.addEventListener("click", function (e) {
   e.preventDefault();
   options = "text";
-  console.log(options);
+  colorOption = "black";
+  circularCursorElement.style.visibility = "hidden";
 });
 
 //Change option to draw
@@ -31,7 +51,8 @@ let drawButton = document.getElementById("drawButton");
 drawButton.addEventListener("click", function (e) {
   e.preventDefault();
   options = "draw";
-  console.log(options);
+  colorOption = "black";
+  circularCursorElement.style.visibility = "hidden";
 });
 
 //adding text to canvas
@@ -61,35 +82,25 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-// Make our in-memory canvas
-var inMemCanvas = document.createElement("canvas");
-var inMemCtx = inMemCanvas.getContext("2d");
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 window.onresize = function () {
-  //repaint everything
   inMemCanvas.width = canvas.width;
   inMemCanvas.height = canvas.height;
   inMemCtx.drawImage(canvas, 0, 0);
-  //
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  //
   ctx.drawImage(inMemCanvas, 0, 0);
 };
 
 ctx = canvas.getContext("2d");
+
 mouseDown = false;
 socket.emit("join", room);
 
-//Erase entire white board
-const clear = document.getElementById("clearButton");
-clear.addEventListener("click", function (e) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
-
 socket.on("draw", function ({ x, y, colorOption }) {
+  if (colorOption === "white") ctx.lineWidth = 20.0;
+  else ctx.lineWidth = 3.0;
   ctx.lineTo(x, y);
   ctx.stroke();
   ctx.strokeStyle = colorOption;
@@ -108,11 +119,8 @@ socket.on("text", function ({ key, firstClickX, firstClickY }) {
 
 window.onmousedown = (e) => {
   if (options === "draw") {
-    //ctx.beginPath();
-    //ctx.moveTo(x, y);
     socket.emit("down", { payload: { x, y, colorOption }, room });
     mouseDown = true;
-    //ctx.closePath();
   }
 };
 
@@ -123,15 +131,16 @@ window.onmouseup = (e) => {
 };
 
 window.onmousemove = (e) => {
+  circularCursorElement.style.left = e.clientX - 20 + "px";
+  circularCursorElement.style.top = e.clientY - 20 + "px";
   if (options === "draw") {
     x = e.clientX;
     y = e.clientY;
-
     if (mouseDown) {
-      socket.emit("draw", { payload: { x, y, colorOption }, room });
-      //ctx.lineTo(x, y);
-      //ctx.stroke();
-      //ctx.strokeStyle = colorOption;
+      socket.emit("draw", {
+        payload: { x, y, colorOption },
+        room,
+      });
     }
   }
 };
