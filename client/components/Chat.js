5e1b248b1248187dd8c2ce10aebc564258d32e7c
adamweinberg <adamcweinberg@gmail.com>
2021-07-12 18:07:27 -0700

    added user count to document title

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -192,7 +192,6 @@ const Chat = () => {
     let splitPathName = pathname.split("+");
     userName = splitPathName[0].substring(1);
     room = splitPathName[1].split("-").join(" ");
-    document.title = room;
 
     socket.emit("join", room);
     loadLastHundredMessages();
@@ -202,9 +201,8 @@ const Chat = () => {
     renderMessage(payload)
   });
 
-  socket.on("userCount", function (userCount) {
-		// let userCountText = document.getElementById("usercount")
-		// userCountText.innerText = "Online Users: (" + userCount + ")"
+  socket.on("userCount", function (userCount, room) {
+    document.title = `${room} (${userCount})`
 	})
 
   function handleReturn(event) {
