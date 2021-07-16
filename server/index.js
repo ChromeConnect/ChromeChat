1c9de6bb9528162db62227c8c1547682b68ed7a9
adamweinberg <adamcweinberg@gmail.com>
2021-07-16 13:42:53 -0700

    user count updates after leaving room

--- server/index.js
+++ server/index.js
@@ -32,6 +32,14 @@ io.on("connection", (socket) => {
 });
 
 io.on("connection", (socket) => {
+  socket.on("disconnecting", () => {
+    const socketioRoom = Array.from(socket.rooms).pop()
+    var userCount = io.sockets.adapter.rooms.get(socketioRoom).size - 1;
+    io.to(socketioRoom).emit("userCount", userCount, socketioRoom);
+  });
+});
+
+io.on("connection", (socket) => {
   socket.on("chat message", (data) => {
     const { payload, socketioRoom } = data;
     io.to(socketioRoom).emit("chat message", payload);
